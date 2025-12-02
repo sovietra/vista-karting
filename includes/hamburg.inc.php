cc6f028ca30644042d53c28a061d5ad40c70b83a
sovietra <167070047+sovietra@users.noreply.github.com>
2025-12-02 12:59:43 +0100

    Add files via upload

---
+++ includes/hamburg.inc.php
@@ -0,0 +1,14 @@
+<div class="hamburger-menu">
+    <div class="menu-icon" onclick="toggleMenu()">&#9776;</div>
+    <div id="togglediv" class="hidden">
+        <nav id="header-div">
+            <div class="button">
+                <div onclick="window.location.assign('index.php');" class="Home" type="button">Home</div>
+                <div onclick="window.location.assign('poules.php');" class="about" type="button">Poules</div>
+                <div onclick="window.location.assign('home.php');" class="projects" type="button">Info</div>
+                <div onclick="window.location.assign('contact-pagina.php');" class="contact" type="button">Contact</div>
+            </div>
+        </nav>
+    </div>
+</div>
+</div>
